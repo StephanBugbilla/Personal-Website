4fbff1046603cbb2f550d986444e64641e1ff8eb
Stephan2378 <sbugbilla@gmail.com>
2024-08-25 02:43:11 +0000

    testing

--- script.js
+++ script.js
@@ -9,11 +9,13 @@ showSlides();
 
 function showSlides() {
     let slides = document.getElementsByClassName("slide");
+    // Remove "active" class from all slides
     for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
+        slides[i].classList.remove("active");
     }
     slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    slides[slideIndex-1].style.display = "block";  
+    if (slideIndex > slides.length) {slideIndex = 1}
+    // Add "active" class to the current slide
+    slides[slideIndex-1].classList.add("active");
     setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
